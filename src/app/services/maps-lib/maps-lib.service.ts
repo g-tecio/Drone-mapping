6f59f7adf2c2d27bb7d2852152a90227835235f1
Marco Mercado Lugo <marcomercadolugo@MacBook-Pro-de-Marco.local>
2018-11-08 10:57:36 -0600

    First CI/CD automated deploy

--- src/app/services/maps-lib/maps-lib.service.ts
+++ src/app/services/maps-lib/maps-lib.service.ts
@@ -179,7 +179,7 @@ export class MapsLibService {
       path: [new google.maps.LatLng(position.lat, position.lng),
       new google.maps.LatLng(destination.lat, destination.lng)
       ],
-      strokeColor: "pink",
+      strokeColor: "blue",
       strokeOpacity: 1.0,
       strokeWeight: 2
     });
